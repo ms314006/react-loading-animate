84d48971de04bb3ec1415416deb9b13ecacf0d71
GQSM <ms314006@hotmail.com>
2020-03-25 20:21:59 +0800

    [ADD] Add position absolute, and custom zIndex to wrapp

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import * as loadingAnimateSource from './loadingAnimate';
 import styles from './index.scss';
 
 const Loading = (props) => {
-  const { type } = props;
+  const { type, zIndex } = props;
   return (
-    <div className={styles.wrapp}>
+    <div
+      className={styles.wrapp}
+      style={{ zIndex }}
+    >
       {loadingAnimateSource[type](props)}
     </div>
   );
@@ -14,10 +17,12 @@ const Loading = (props) => {
 
 Loading.propTypes = {
   type: PropTypes.string,
+  zIndex: PropTypes.number,
 };
 
 Loading.defaultProps = {
   type: 'beat',
+  zIndex: 10,
 };
 
 export default Loading;
