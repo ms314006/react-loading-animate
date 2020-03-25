30ad843d9a9b222f6132ec719f5990d19c3a7129
GQSM <ms314006@hotmail.com>
2020-03-25 15:45:16 +0800

    [ADD] Add animate type of Circle

---
+++ src/loadingAnimate/Circle/Circle.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.scss';
+
+const Circle = (props) => {
+  const { fill, diameter } = props;
+  return (
+    <div
+      className={styles.ring}
+      style={{
+        border: `7px dashed ${fill}`,
+        height: diameter,
+        width: diameter,
+      }}
+    />
+  );
+};
+
+Circle.propTypes = {
+  fill: PropTypes.string,
+  diameter: PropTypes.number,
+};
+
+Circle.defaultProps = {
+  fill: '#4b9cdb',
+  diameter: 15,
+};
+
+export default Circle;
--- src/loadingAnimate/index.js
+++ src/loadingAnimate/index.js
@@ -1,3 +1,4 @@
 export { default as beat } from './Beat';
 export { default as bar } from './Bar';
 export { default as creeping } from './Creeping';
+export { default as circle } from './Circle';
